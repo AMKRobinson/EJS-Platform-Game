63bbd9f961c1524c94ecd72134863ad1474c9477
Anthony Robinson <robinsonamk@gmail.com>
2019-01-17 16:49:26 -0800

    update

--- src/platform-game.js
+++ src/platform-game.js
@@ -52,6 +52,7 @@ class Level {
 
     this.rows = rows.map((row, y) => {
       return row.map((ch, x) => {
+        let type = levelChars[ch];
         if(typeof type === 'string') return type;
         this.startActors.push(
           type.create(new Vec(x, y), ch));
@@ -84,8 +85,8 @@ class Vec {
   plus(other) {
     return new Vec(this.x + other.x, this.y + other.y);
   }
-  times(factors) {
-    return new Vec(this.x * factor, this.y * factor)
+  times(factor) {
+    return new Vec(this.x * factor, this.y * factor);
   }
 }
 
@@ -99,8 +100,128 @@ class Player {
 
   static create(pos) {
     return new Player(pos.plus(new Vec(0, -0.5)),
-    new Vec(0, 0));
+      new Vec(0, 0));
   }
 }
 
 Player.prototype.size = new Vec(0.8, 1.5);
+
+class Lava {
+  constructor(pos, speed, reset) {
+    this.pos = pos;
+    this.speed = speed;
+    this.reset = reset;
+  }
+
+  get type() { return 'lava'; }
+
+  static create(pos, ch) {
+    if (ch === '=') {
+      return new Lava(pos, new Vec(2, 0));
+    } else if (ch === '|') {
+      return new Lava(pos, new Vec(0, 2));
+    } else if (ch === 'v') {
+      return new Lava(pos, new Vec(0, 3), pos);
+    }
+  }
+}
+
+Lava.prototype.size = new Vec(1, 1);
+
+class Coin {
+  constructor(pos, basePos, wobble) {
+    this.pos = pos;
+    this.basePos = basePos;
+    this.wobble = wobble;
+  }
+
+  get type() { return 'coin'; }
+
+  static create(pos) {
+    let basePos = pos.plus(new Vec(0.2, 0.1));
+    return new Coin(basePos, basePos,
+      Math.random() * Math.PI * 2);
+  }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+const levelChars = {
+  '.': 'empty',
+  '#': 'wall',
+  '+': 'lava',
+  '@': Player,
+  'o': Coin,
+  '=': Lava,
+  '|': Lava,
+  'v': Lava,
+};
+
+let simpleLevel = new Level(simpleLevelPlan);
+console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+class DOMDisplay {
+  constructor(parent, level) {
+    this.dom = elt('div', {class: 'game'}, drawGrid(level));
+    this.actorLayer = null;
+    parent.appendChild(this.dom);
+  }
+
+  clear() { this.dom.remove(); }
+}
+
+const scale = 20;
+
+function drawGrid(level) {
+  return elt('table', {
+    class: 'background',
+    style: `width: ${level.width * scale}px`,
+  }, ...level.row.map(row =>
+    elt('tr', {style: `height: $ ${scale}px`},
+      ...row.map(type => elt('td', {class: type})))
+  ));
+}
+
+function drawActors(actors) {
+  return elt('div', {}, ...actors.map(actor => {
+    let rect = elt('div', {class: `actor ${actor.type}`});
+    rect.style.width = `${actor.size.x * scale}px`;
+    rect.style.height = `${actor.size.y * scale}px`;
+    rect.style.left = `${actor.pos.x * scale}px`;
+    rect.style.top = `${actor.pos.y * scale}px`;
+    return rect;
+  }));
+}
+
+DOMDisplay.prototype.setState = function(state) {
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+};
+
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let margin = width / 3;
+
+  // Viewport
+
+  let left = this.dom.scrollLeft, right = left + width;
+  let top = this.dom.scrollTop, bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos.plus(player.size.times(0.5))
+}
